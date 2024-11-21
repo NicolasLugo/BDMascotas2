0295b87b24ce55a2e76501c2de7df83f6c2c9c10
General <Miguelleytonlml@outlook.com>
2024-11-21 16:35:01 -0300

    Proyecto completado con envío de datos a Firestore

--- app/src/main/java/com/example/bdmascotas2/MainActivity.java
+++ app/src/main/java/com/example/bdmascotas2/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.bdmascotas2;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+//import com.google.firebase.firestore.core.View;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private EditText txtCodigo, txtNombre, txtDueno, txtDireccion;
@@ -25,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cargarListaFirestore();
 
         db = FirebaseFirestore.getInstance();
 
@@ -41,7 +56,60 @@ public class MainActivity extends AppCompatActivity {
         spMascota.setAdapter(spinnerAdapter);
     }
 
-    public void cargarListaFirestore(){
+    public void enviarDatosFirestore(View view){
+        String codigo = txtCodigo.getText().toString();
+        String nombre = txtNombre.getText().toString();
+        String dueno = txtDueno.getText().toString();
+        String direccion = txtDireccion.getText().toString();
+        String tipoMascota = spMascota.getSelectedItem().toString();
 
+        Map<String, Object> mascota = new HashMap<>();
+        mascota.put("codigo", codigo);
+        mascota.put("nombre", nombre);
+        mascota.put("dueño", dueno);
+        mascota.put("direccion", direccion);
+        mascota.put("tipoMascota", tipoMascota);
+
+        db.collection("mascotas")
+                .document(codigo)
+                .set(mascota)
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(MainActivity.this, "Datos enviados a Firestore correctamente", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                   Toast.makeText(MainActivity.this, "Error al enviar datos a Firestore: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+
+    }
+
+    public void cargarLista(View view){
+        cargarListaFirestore();
+    }
+
+    public void cargarListaFirestore(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("mascotas")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            List<String> listaMascotas = new ArrayList<>();
+                            for(QueryDocumentSnapshot document : task.getResult()){
+                                String linea = "||" + document.getString("codigo") + "||" +
+                                        document.getString("nombre") + "||" +
+                                        document.getString("dueño") + "||" +
+                                        document.getString("direccion") + "||";
+                                listaMascotas.add(linea);
+                            }
+
+                            ArrayAdapter<String> adaptador = new ArrayAdapter<>(MainActivity.this,
+                                    android.R.layout.simple_list_item_1, listaMascotas);
+                            lista.setAdapter(adaptador);
+                        } else {
+                            Log.e("TAG", "Error al obtener datos de Firestore", task.getException());
+                        }
+                    }
+                });
     }
 }
